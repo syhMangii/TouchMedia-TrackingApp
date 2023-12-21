ec15dae083020c516806d0d8d5e2f20c85182c8b
syhMangii <aisyahazman0231@gmail.com>
2023-12-22 00:09:52 +0800

    first commit

---
+++ app/Http/Controllers/TemplateController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TemplateController extends Controller
+{
+    public function index(){
+        return view('frontend.home');
+    }
+    public function userHome(){
+        return view('frontend.user.homepage');
+    }
+}
