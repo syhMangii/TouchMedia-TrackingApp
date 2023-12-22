544e7998a5fb06e5f96d99d61eec4a84f0ab1a8f
syhMangii <aisyahazman0231@gmail.com>
2023-12-23 02:19:27 +0800

    delete text

--- resources/views/frontend/user/homepage.blade.php
+++ resources/views/frontend/user/homepage.blade.php
@@ -7,7 +7,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Topic Detail Page</title>
+        <title>Topic Detail </title>
 
         <!-- CSS FILES -->        
         <link rel="preconnect" href="https://fonts.googleapis.com">
